de324fd8550892884b672c18474b3266ab065023
huangtao00 <397916230@qq.com>
2018-09-30 22:17:32 +0800

    brower plugin;vps json'

--- shell/exlogin.sh
+++ shell/exlogin.sh
@@ -3,7 +3,7 @@ set timeout 50
 # spawn "./loginjpan.sh"
  set username root
  set ip 45.76.194.104
-set password  "yourpassword"
+set password  ""
 spawn ssh ${username}@${ip}
 expect "*password:"  {send "$password\r"}
 expect "Last login:"
